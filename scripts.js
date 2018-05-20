02323703e3ce8da4dfb59b81d2fcbfdef037a7d1
samalws <saalws@ctemc.org>
2018-05-19 23:14:32 -0400

    center

--- scripts.js
+++ scripts.js
@@ -1,20 +1,29 @@
 $(document).ready(function(){
-	function makeBox(boxes){
-		for (var i=0;i<boxes;i++){
+	boxes=5;
+	function makeBox(posts){
+		for (var i=1;i<boxes+1;i++){
 			var box = document.createElement('div');
 			$(box).addClass('box');
 			$(box).css('top',String(
-				50+30*Math.sin(6.28318530718*i*2/boxes)
+				50+30*Math.sin(6.28318530718*i/(boxes+1))
 			)+'%');
 			$(box).css('right',String(
-				50+30*Math.cos(6.28318530718*i*2/boxes)
+				50+30*Math.cos(6.28318530718*i/(boxes+1))
 			)+'%');
-			box.innerHTML = 'w0w';
+			box.innerHTML = posts[i-1][1];
 			document.getElementsByTagName('body')[0].appendChild(box);
 		}
 	}
-	$("#centered").click(function(){
-		$(this).animate({left: "50px",width: "50px", height: "50px"});
+	function moveLeft(x) {
+		x.animate({top:"50%",
+			left: "175px",
+			width: "50px",
+			height: "50px",
+			margin: "-25px 0 0 -25px"
+		});
+	}
+	$("#center").click(function(){
+		moveLeft($(this));
 	});
-	makeBox(5);
+	makeBox([['','a'],['','b'],['','c'],['','d'],['','e']]);
 })
